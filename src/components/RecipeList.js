813ac7d63bcc742a18f444970ba4d079b4cecde9
kapilesh <kapilesh1@gmail.com>
2022-09-16 20:39:57 +0530

    custom hook - fetch hook

---
+++ src/components/RecipeList.js
@@ -0,0 +1,15 @@
+import './RecipeList.css'
+
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function RecipeList({recipes}) {
+  return (
+    <div className='recipe-list'>{recipes.map(recipe => <div key={recipe.id} className="card">
+        <h1>{recipe.title}</h1>
+    <p>{recipe.cookingTime} to make .</p>
+    <div>{recipe.method.substring(0,100)}...</div>
+    <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
+    </div>)}</div>
+  )
+}
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import './Home.css'
+import useFetch from '../../hooks/useFetch'
+import RecipeList from '../../components/RecipeList'
 
 export default function Home() {
-  console.log("Hello welof")
+  const {error,isPending,data} = useFetch("http://localhost:3000/recipes")
   return (
-    <div>Home</div>
+    <div className='home'>
+      {error && <div className='error'>{error}</div>}
+      {isPending && <div className='loading'>loading...</div>}
+      {data && <RecipeList recipes={data}/>}
+    </div>
   )
 }
